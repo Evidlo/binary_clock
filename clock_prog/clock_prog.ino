d429b7f662ec25afec834cd41fb3077d18059160
Evan <evan@evanw.org>
2015-07-18 19:35:02 -0600

    fixed issue with first digit in hours

--- clock_prog/clock_prog.ino
+++ clock_prog/clock_prog.ino
@@ -1,5 +1,5 @@
-#Evan Widloski - 2015-07-18
-#Binary Clock Programmer - Sets time of RTC module on binary clock
+//Evan Widloski - 2015-07-18
+//Binary Clock Programmer - Sets time of RTC module on binary clock
 
 #include <USI_TWI_Master.h>
 #include <TinyWireM.h>
@@ -14,8 +14,8 @@
 
 void setup() {
   int second = 0;
-  int minute = 14;
-  int hour = 4;
+  int minute = 27;
+  int hour = 7;
   int dayOfWeek = 0;
   int dayOfMonth = 18;
   int month = 7;
@@ -23,13 +23,13 @@ void setup() {
   TinyWireM.begin();
   TinyWireM.beginTransmission(0x68);
   TinyWireM.write(0);
-  TinyWireM.write(((second/10)<<4) + second%10);
-  TinyWireM.write(((minute/10)<<4) + minute%10);
-  TinyWireM.write((1<<6)+ ((hour/12)<<5) + ((hour/10)<<4) + hour%10);
-  TinyWireM.write(1);
-  TinyWireM.write(((dayOfMonth/10)<<4) + dayOfMonth%10);
-  TinyWireM.write(((month/10)<<4) + month%10);
-  TinyWireM.write(((year/10)<<4) + year%10);
+  TinyWireM.write(((second/10)<<4) + second%10);                                     //write seconds
+  TinyWireM.write(((minute/10)<<4) + minute%10);                                     //write minutes
+  TinyWireM.write((1<<6) + (1<<5) + ((hour/12)<<5) + ((hour/10)<<4) + hour%10);      //set to 12h mode, set pm, and write hour;
+  TinyWireM.write(1);                                                                //write arbitrary day of week
+  TinyWireM.write(((dayOfMonth/10)<<4) + dayOfMonth%10);                             //write day of month
+  TinyWireM.write(((month/10)<<4) + month%10);                                       //write month
+  TinyWireM.write(((year/10)<<4) + year%10);                                         //write 2 digit year
   TinyWireM.endTransmission();
 
 
