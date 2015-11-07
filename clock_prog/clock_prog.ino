34a909e70b2581f8b40ea771be792a777e8e70a6
Evan Widloski <evan@evanw.org>
2015-11-07 16:20:40 -0500

    add day of week

--- clock_prog/clock_prog.ino
+++ clock_prog/clock_prog.ino
@@ -14,11 +14,11 @@
 
 void setup() {
   int second = 0;
-  int minute = 27;
-  int hour = 7;
-  int dayOfWeek = 0;
-  int dayOfMonth = 18;
-  int month = 7;
+  int minute = 20;
+  int hour = 4;
+  int dayOfWeek = 6;
+  int dayOfMonth = 7;
+  int month = 11;
   int year = 15; 
   TinyWireM.begin();
   TinyWireM.beginTransmission(0x68);
@@ -26,7 +26,7 @@ void setup() {
   TinyWireM.write(((second/10)<<4) + second%10);                                     //write seconds
   TinyWireM.write(((minute/10)<<4) + minute%10);                                     //write minutes
   TinyWireM.write((1<<6) + (1<<5) + ((hour/12)<<5) + ((hour/10)<<4) + hour%10);      //set to 12h mode, set pm, and write hour;
-  TinyWireM.write(1);                                                                //write arbitrary day of week
+  TinyWireM.write(dayOfWeek);                                                        //write arbitrary day of week
   TinyWireM.write(((dayOfMonth/10)<<4) + dayOfMonth%10);                             //write day of month
   TinyWireM.write(((month/10)<<4) + month%10);                                       //write month
   TinyWireM.write(((year/10)<<4) + year%10);                                         //write 2 digit year
